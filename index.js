2c262050bb596a15175c385499537a96ede93576
zhangliang <zl7511555@gmail.com>
2022-12-05 17:57:38 +0800

    fix the origin repository problem: it depend interface of three.js but does not import three.js in source in case of using with npm

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
  * @author Takahiro / https://github.com/takahirox
  */
 
+/*
+import * as THREE from 'three';
+
 if ( typeof THREE === 'undefined' ) {
 
 	throw new Error( 'THREE.ZipLoader: Import Three.js https://github.com/mrdoob/three.js/ ' +
 	                 'before loading ZipLoader.' );
 
 }
+*/
 
 import { ZipLoader } from './src/ZipLoader.js';
 import { ZipLoadingManager } from './src/ZipLoadingManager.js';
 
+/*
 THREE.ZipLoader = ZipLoader;
 THREE.ZipLoadingManager = ZipLoadingManager;
+*/
+
+export { ZipLoadingManager };
--- src/ZipLoadingManager.js
+++ src/ZipLoadingManager.js
@@ -2,60 +2,66 @@
  * @author Takahiro / https://github.com/takahirox
  */
 
-function ZipLoadingManager( onLoad, onProgress, onError ) {
-
-	THREE.LoadingManager.call( this, onLoad, onProgress, onError );
-
-}
-
-ZipLoadingManager.prototype = Object.assign( Object.create( THREE.LoadingManager.prototype ), {
-
-	constructor: ZipLoadingManager
-
-} );
-
-ZipLoadingManager.uncompress = function ( url, exts, onLoad, onProgress, onError ) {
-
-	var manager = new THREE.ZipLoadingManager( onLoad, onProgress, onError );
-
-	if ( url.match( /\.zip$/ ) === null ) return Promise.resolve( { urls: [ url ], manager: manager } );
-
-	if ( ! Array.isArray( exts ) ) exts = [ exts ];
-
-	var str = '(';
-
-	for ( var i = 0, il = exts.length; i < il; i ++ ) {
-
-		if ( i > 0 ) str += '|';
-
-		str += exts[ i ].replace( '.', '\\.' );
-
-	}
-
-	str += ')$';
-
-	return new THREE.ZipLoader( manager ).load( url ).then( function ( zip ) {
-
-		var files = zip.find( new RegExp( str, 'i' ) );
-
-		if ( files.length === 0 ) {
-
-			return Promise.reject( new Error( 'THREE.ZipLoadingManager: No ' + exts.join( ', ' ) + ' file in ' + url ) );
-
-		}
-
-		manager.setURLModifier( zip.urlResolver );
-
-		return {
-
-			urls: files,
-			manager: manager
-
-		};
-
-	} ).catch( onError );
-
-};
-
-
-export { ZipLoadingManager };
+ import { LoadingManager } from 'three';
+ import { ZipLoader } from './ZipLoader.js';
+ 
+ function ZipLoadingManager( onLoad, onProgress, onError ) {
+ 
+	 LoadingManager.call( this, onLoad, onProgress, onError );
+ 
+ }
+ 
+ ZipLoadingManager.prototype = Object.assign( Object.create( LoadingManager.prototype ), {
+ 
+	 constructor: ZipLoadingManager
+ 
+ } );
+ 
+ ZipLoadingManager.uncompress = function ( url, exts, onLoad, onProgress, onError ) {
+ 
+	 // var manager = new THREE.ZipLoadingManager( onLoad, onProgress, onError );
+	 var manager = new ZipLoadingManager( onLoad, onProgress, onError );
+ 
+	 if ( url.match( /\.zip$/ ) === null ) return Promise.resolve( { urls: [ url ], manager: manager } );
+ 
+	 if ( ! Array.isArray( exts ) ) exts = [ exts ];
+ 
+	 var str = '(';
+ 
+	 for ( var i = 0, il = exts.length; i < il; i ++ ) {
+ 
+		 if ( i > 0 ) str += '|';
+ 
+		 str += exts[ i ].replace( '.', '\\.' );
+ 
+	 }
+ 
+	 str += ')$';
+ 
+	 // return new THREE.ZipLoader( manager ).load( url ).then( function ( zip ) {
+	 return new ZipLoader( manager ).load( url ).then( function ( zip ) {
+ 
+		 var files = zip.find( new RegExp( str, 'i' ) );
+ 
+		 if ( files.length === 0 ) {
+ 
+			 return Promise.reject( new Error( 'THREE.ZipLoadingManager: No ' + exts.join( ', ' ) + ' file in ' + url ) );
+ 
+		 }
+ 
+		 manager.setURLModifier( zip.urlResolver );
+ 
+		 return {
+ 
+			 urls: files,
+			 manager: manager
+ 
+		 };
+ 
+	 } ).catch( onError );
+ 
+ };
+ 
+ 
+ export { ZipLoadingManager };
+ 
